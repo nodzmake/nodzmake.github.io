7788e6905f2c62da73123d0ed0b488f213d14d08
noah <ibrahimkhurshid09@gmail.com>
2023-08-16 19:58:07 +0500

    react router issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppRouter from './Routes'
 import './index.css'
-
 import reportWebVitals from './reportWebVitals'
+import App from './App'
+import Sheet from './components/Sheet'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/sheet' element={<Sheet />} />
+    </Routes>
+  </BrowserRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
